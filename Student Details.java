02b74fa144028d19ca7be3ad8afd548dcf07cc84
Tadbir Tareq G2 <146854510+Tadbir20@users.noreply.github.com>
2025-01-25 17:04:15 +0600

    Create Student Details.java

---
+++ Student Details.java
@@ -0,0 +1,28 @@
+public class StudentDetailsJava {
+
+    public static void main(String[] args) {
+     // Create variables of different data types
+        String studentName = "Tadbir Tareq Jitu";
+        long studentID = 22235103020l;
+        long phoneNum = 1775894160l;
+        int age = 24;
+        String universityName = "BUBT";
+        String homeLocation = "Mirpur-2";
+        String deperment = "CSE";
+        float hight = 5.5f;
+        char bloodGroup = 'B';
+        
+        
+
+        // Print variables
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("University Name: " + universityName);
+        System.out.println("Address: " + homeLocation);
+        System.out.println("Deperment: " + deperment);
+        System.out.println("Hight: " + hight + "'");
+        System.out.println("Blood: " + bloodGroup + "+");
+        System.out.println("Phone Number: 0" + phoneNum);
+    }
+}
